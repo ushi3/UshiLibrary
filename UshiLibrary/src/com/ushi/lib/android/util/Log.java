b2b4448f0394bbd1d1868f4146db0037cd6281b1
ushi3 <ushi3.jp@gmail.com>
2012-12-03 17:02:52 +0900

    Logのメソッドを増やした
    ・Log.i()のラッパを追加
    ・引数をObject型に対応

--- UshiLibrary/src/com/ushi/lib/android/util/Log.java
+++ UshiLibrary/src/com/ushi/lib/android/util/Log.java
@@ -3,6 +3,27 @@ package com.ushi.lib.android.util;
 public class Log {
 
 	/**
+	 * {@link android.util.Log#i(String, String)} のラッパ.
+	 *
+	 * @param msg
+	 *            出力するメッセージ
+	 */
+	public static void i(String msg) {
+		android.util.Log.i(StackUtil.getClassName(1), msg);
+	}
+
+	/**
+	 * {@link android.util.Log#i(String, String)} のラッパ. 現在のソースコード上の位置を出力する.
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 */
+	public static void i(Object ob) {
+		android.util.Log.i(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null);
+	}
+
+	/**
 	 * {@link android.util.Log#d(String, String)} のラッパ.
 	 *
 	 * @param msg
@@ -16,7 +37,19 @@ public class Log {
 	 * {@link android.util.Log#d(String, String)} のラッパ. 現在のソースコード上の位置を出力する.
 	 */
 	public static void d() {
-		android.util.Log.d(StackUtil.getClassName(1), StackUtil.getMethodName(1));
+		android.util.Log.d(StackUtil.getClassName(1),
+				StackUtil.getMethodName(1));
+	}
+
+	/**
+	 * {@link android.util.Log#d(String, String)} のラッパ. 現在のソースコード上の位置を出力する.
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 */
+	public static void d(Object ob) {
+		android.util.Log.d(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null);
 	}
 
 	/**
@@ -27,6 +60,17 @@ public class Log {
 	 */
 	public static void wtf(String msg) {
 		android.util.Log.wtf(StackUtil.getClassName(1), msg);
+	}
+
+	/**
+	 * {@link android.util.Log#wtf(String, String)} のラッパ.
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 */
+	public static void wtf(Object ob) {
+		android.util.Log.wtf(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null);
 	}
 
 	/**
@@ -42,12 +86,26 @@ public class Log {
 	/**
 	 * {@link android.util.Log#w(String, String, Throwable)} のラッパ。
 	 *
-	 * @param msg 出力するメッセージ
+	 * @param msg
+	 *            出力するメッセージ
 	 * @param th
 	 *            発生した例外
 	 */
 	public static void w(String msg, Throwable th) {
 		android.util.Log.w(StackUtil.getClassName(1), msg, th);
+	}
+
+	/**
+	 * {@link android.util.Log#w(String, String, Throwable)} のラッパ。
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 * @param th
+	 *            発生した例外
+	 */
+	public static void w(Object ob, Throwable th) {
+		android.util.Log.w(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null, th);
 	}
 
 	/**
@@ -61,6 +119,17 @@ public class Log {
 	}
 
 	/**
+	 * {@link android.util.Log#w(String, String)} のラッパ。
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 */
+	public static void w(Object ob) {
+		android.util.Log.w(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null);
+	}
+
+	/**
 	 * {@link android.util.Log#d(String, String)} のラッパ。
 	 *
 	 * @param msg
@@ -68,6 +137,17 @@ public class Log {
 	 */
 	public static void e(String msg) {
 		android.util.Log.e(StackUtil.getClassName(1), msg);
+	}
+
+	/**
+	 * {@link android.util.Log#d(String, String)} のラッパ。
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 */
+	public static void e(Object ob) {
+		android.util.Log.e(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null);
 	}
 
 	/**
@@ -83,11 +163,25 @@ public class Log {
 	/**
 	 * {@link android.util.Log#e(String, String, Throwable)} のラッパ。
 	 *
-	 * @param msg 出力するメッセージ
+	 * @param msg
+	 *            出力するメッセージ
 	 * @param th
 	 *            発生した例外
 	 */
 	public static void e(String msg, Throwable th) {
 		android.util.Log.e(StackUtil.getClassName(1), msg, th);
+	}
+
+	/**
+	 * {@link android.util.Log#e(String, String, Throwable)} のラッパ。
+	 *
+	 * @param ob
+	 *            ob.toString() or null
+	 * @param th
+	 *            発生した例外
+	 */
+	public static void e(Object ob, Throwable th) {
+		android.util.Log.e(StackUtil.getClassName(1),
+				ob != null ? ob.toString() : null, th);
 	}
 }
