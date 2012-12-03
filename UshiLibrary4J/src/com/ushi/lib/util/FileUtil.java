5fca5ed126a39dd9998f9b5be1482d745e0e5b84
ushi3 <ushi3.jp@gmail.com>
2012-12-03 23:44:28 +0900

    FileUtilのファイルコピーを追加
    ・FileChannelを引数にできるものを追加
    ・InputStreamとOutputStreamに対応

--- UshiLibrary4J/src/com/ushi/lib/util/FileUtil.java
+++ UshiLibrary4J/src/com/ushi/lib/util/FileUtil.java
@@ -1,32 +1,38 @@
 package com.ushi.lib.util;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.channels.FileChannel;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class FileUtil {
 
-	private static Logger logger = Logger.getLogger(FileUtil.class
-			.getSimpleName());
+	private static Logger logger = Logger.getLogger(FileUtil.class.getSimpleName());
 
 	private FileUtil() {
 	}
 
 	/**
 	 * ファイルをコピーする。
-	 *
-	 * @param fromPath コピー元ファイルパス
-	 * @param toPath コピー先ファイルパス
+	 * 
+	 * @param fromPath
+	 *            コピー元ファイルパス
+	 * @param toPath
+	 *            コピー先ファイルパス
 	 * @return コピーできたらtrue
 	 */
 	public static boolean copy(String fromPath, String toPath) {
 		if (fromPath == null || toPath == null) {
-			logger.log(Level.SEVERE, "files if null. " + fromPath + " -> " + toPath);
+			logger.log(Level.SEVERE, "filePath is null. " + fromPath + " -> " + toPath);
 			return false;
 		}
 
@@ -35,52 +41,146 @@ public class FileUtil {
 
 	/**
 	 * ファイルをコピーする。
-	 *
-	 * @param from コピー元
-	 * @param to コピー先
+	 * 
+	 * @param fromPath
+	 *            コピー元ファイルパス
+	 * @param toPath
+	 *            コピー先ファイルパス
 	 * @return コピーできたらtrue
 	 */
-	public static boolean copy(File from, File to) {
+	public static boolean copy(FileInputStream from, FileOutputStream to) {
 		if (from == null || to == null) {
-			logger.log(Level.SEVERE, "files if null. " + from + " -> " + to);
+			logger.log(Level.SEVERE, "streams is null. " + from + " -> " + to);
 			return false;
 		}
 
-		FileChannel out = null;
-		FileChannel in = null;
 		try {
-			out = new FileOutputStream(to).getChannel();
-			in = new FileInputStream(from).getChannel();
+			return copy(from.getChannel(), to.getChannel());
+		} finally {
+			close(from, to);
+		}
+	}
 
+	/**
+	 * ファイルをコピーする。
+	 * 
+	 * @param fromCh
+	 *            コピー元ファイルチャネル
+	 * @param toCh
+	 *            コピー先ファイルチャネル
+	 * @return
+	 */
+	public static boolean copy(FileChannel fromCh, FileChannel toCh) {
+		if (fromCh == null || toCh == null) {
+			logger.log(Level.SEVERE, "channels is null. " + fromCh + " -> " + toCh);
+			close(fromCh, toCh);
+			return false;
+		}
+
+		try {
 			long pos = 0;
-			long size = in.size() - pos;
+			long size = fromCh.size() - pos;
 
-			return out.transferFrom(in, pos, size) == size;
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			return toCh.transferFrom(fromCh, pos, size) == size;
 
 		} catch (IOException e) {
 			e.printStackTrace();
 
 		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			try {
-				if (in != null) {
-					in.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			close(fromCh, toCh);
 		}
 
 		return false;
 	}
 
+	/**
+	 * ファイルをコピーする。
+	 * 
+	 * @param from
+	 *            コピー元
+	 * @param to
+	 *            コピー先
+	 * @return コピーできたらtrue
+	 */
+	public static boolean copy(File from, File to) {
+		if (from == null || to == null) {
+			logger.log(Level.SEVERE, "files is null. " + from + " -> " + to);
+			return false;
+		}
+
+		FileInputStream in = null;
+		FileOutputStream out = null;
+
+		try {
+			in = new FileInputStream(from);
+			out = new FileOutputStream(to);
+			return copy(in.getChannel(), out.getChannel());
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+
+		} finally {
+			close(in, out);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Stream の内容をコピーします。
+	 * 
+	 * @param from コピー元
+	 * @param to コピー先
+	 * @return
+	 */
+	public static boolean copy(InputStream from, OutputStream to) {
+		if (from == null || to == null) {
+			logger.log(Level.SEVERE, "args is null. " + from + " -> " + to);
+			return false;
+		}
+
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		try {
+			bis = new BufferedInputStream(from);
+			bos = new BufferedOutputStream(to);
+			
+			byte[] bin = new byte[1024];
+			while (bis.read(bin, 0, bin.length) != -1) {
+				bos.write(bin);
+			}
+			
+			bos.flush();
+			return true;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		} finally {
+			close(bis, bos, from, to);
+		}
+
+		return false;
+	}
+
+	/**
+	 * 引数の {@link Closeable} をcloseする。
+	 * 
+	 * @param closes
+	 */
+	public static void close(Closeable... closes) {
+		if (closes == null) {
+			return;
+		}
+
+		for (Closeable c : closes) {
+			try {
+				if (closes != null) {
+					c.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
